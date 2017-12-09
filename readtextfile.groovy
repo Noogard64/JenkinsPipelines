84d6b3ac3c3eaa941b26afdcd06d425b014b83ee
Sean Phillips <sean-phillips@live.com>
2017-12-09 11:51:59 -0500

    added new pipeline

---
+++ readtextfile.groovy
@@ -0,0 +1,17 @@
+pipeline{
+    options{timestamps()}
+    agent{label master}
+    stages{
+        stage('read data from text file'){
+            script{
+                def words = []
+                new File('C:\\Users\\Sean\\Desktop\\textFile.txt') eachline{ line -> words << line}
+                
+                words.each{
+                    bat 'echo '& it
+                }
+            }
+        }
+    }
+
+}
